1cc09e751280b5d1e03274bcef36353556cbf1f4
oryweaver <78379362+oryweaver@users.noreply.github.com>
2024-01-22 13:19:01 -0500

    Create minka-aire-receive_test.ino
    Added sketch to see raw messages from remote control. Automatically adds leading zeros in binary to match length of bits in transmission.

---
+++ helpers/minka-aire-receive_test/minka-aire-receive_test.ino
@@ -0,0 +1,63 @@
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#include <RCSwitch.h>
+#include <ESP8266WiFi.h>
+
+#ifdef ESP32 // for esp32! Receiver on GPIO pin 4. Transmit on GPIO pin 2.
+  #define RX_PIN 4 
+  #define TX_PIN 2
+#elif ESP8266  // for esp8266! Receiver on pin 4 = D2. Transmit on pin 5 = D1.
+  #define RX_PIN 4
+  #define TX_PIN 5
+#else // for Arduino! Receiver on interrupt 0 => that is pin #2. Transmit on pin 6.
+  #define RX_PIN 0
+  #define TX_PIN 6
+#endif 
+
+// Set CC1101 frequency
+// 303.808 determined from FCC Filing https://fccid.io/2AHC3CR400T
+#define FREQUENCY     303.808
+
+// RC-switch settings
+#define RF_PROTOCOL 6
+#define RF_REPEATS  8
+
+int long value;      // int to save value
+int bits;           // int to save bit number
+int prot;          // int to save Protocol number
+
+RCSwitch mySwitch = RCSwitch();
+
+void printBinaryWithLeadingZeros(unsigned long num) {
+  for (int i = bits - 1; i >= 0; i--) {
+    Serial.print((num & (1ul << i)) ? '1' : '0');
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  ELECHOUSE_cc1101.Init();
+  ELECHOUSE_cc1101.setMHZ(FREQUENCY);
+  ELECHOUSE_cc1101.SetRx();
+  mySwitch.enableReceive(RX_PIN);
+}
+
+void loop() {
+    if (mySwitch.available()) {
+    value =  mySwitch.getReceivedValue();        // save received Value
+    prot = mySwitch.getReceivedProtocol();     // save received Protocol
+    bits = mySwitch.getReceivedBitlength();     // save received Bitlength
+
+    Serial.print(prot);
+    Serial.print(" - ");
+    printBinaryWithLeadingZeros(value);
+    Serial.print(" - ");
+    Serial.println(bits);
+
+
+    
+    mySwitch.resetAvailable();
+    }
+
+
+}
